eb78df747e33dad2a24f00809cf52362559dc435
priyatham-k <priyatham7k@gmail.com>
2024-09-10 12:22:34 -0500

    initial project 2

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const app = express();
 app.use(express.json())
 
 app.listen(3001, ()=>{
-    console.log('listening on port 3000');
+    console.log('Enrollment system is listening on port 3000');
 })
