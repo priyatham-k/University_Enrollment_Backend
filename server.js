4e867bc4eedf2c8706e801e42010cc8cf7d61a6e
priyatham-k <priyatham7k@gmail.com>
2024-09-19 01:25:14 -0500

    register and login api

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+// server.js
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/user');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.json())
+// Middleware
+app.use(bodyParser.json());
+app.use('/api/user', authRoutes);
 
-app.listen(3001, ()=>{
-    console.log('Enrollment system is listening on port 3000');
+// Connect to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/University_management', {
 })
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
