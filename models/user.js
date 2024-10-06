0fbf5ec18cc457ca0e6bdfd166531d6a17f9b628
priyatham-k <priyatham7k@gmail.com>
2024-10-06 02:34:51 -0500

    user model update

--- models/user.js
+++ models/user.js
@@ -1,12 +1,18 @@
 // models/user.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['student', 'instructor', 'admin'], required: true }
+  role: {
+    type: String,
+    enum: ["student", "instructor", "admin"],
+    required: true,
+  },
+  enrolledCourses: [{ type: mongoose.Schema.Types.Mixed }],
+  payment: [{ courceName: String, payment: String }],
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
